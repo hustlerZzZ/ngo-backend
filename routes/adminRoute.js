05cd000547ccdda5e7e3614344e701893e4bd861
hustlerZzZ <rishabpal2000@gmail.com>
2024-12-07 11:28:19 +0530

    Added more security to admin route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,15 +1,20 @@
 import express from "express";
-import {deleteUser, getAllUsers, getUser} from "../controller/adminController.js";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+} from "../controller/adminController.js";
+import { restrictTo } from "../controller/authController.js";
 
 const router = express.Router();
 
 // Getting a user using id
-router.get("/get-user/:id", getUser);
+router.get("/get-user/:id", restrictTo, getUser);
 
 // Getting all the user's present on the user
-router.get("/getAllUsers", getAllUsers);
+router.get("/getAllUsers", restrictTo, getAllUsers);
 
 // Delete a user
-router.delete("/delete-user/:id", deleteUser);
+router.delete("/delete-user/:id", restrictTo, deleteUser);
 
 export default router;
